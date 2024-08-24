a4cc345ff2d0f278e204df17ec3fcd6fc401d506
Argens <argens1203@gmail.com>
2024-08-25 00:33:30 +1000

    todo: laggy ui

--- core/visualization.py
+++ core/visualization.py
@@ -22,16 +22,21 @@ plotly.offline.init_notebook_mode(connected=True)
 class RegrMagic(object):
     def __init__(self, game):
         self.game = game
+        self.timeout = 0.5
 
-    def __call__(self):
-        time.sleep(0.2)
-        self.game.step(learn=False)
-
+    def get_info(self):
         info = self.game.get_agent_info()
         items = self.game.get_untaken_items()
         tot_reward = self.game.total_reward
-
         return info, items, tot_reward
+
+    def set_timeout(self, timeout):
+        self.timeout = timeout
+
+    def __call__(self):
+        time.sleep(self.timeout)
+        self.game.step(learn=False)
+        return self.get_info()
 
 
 class Visualization:
@@ -51,25 +56,16 @@ class Visualization:
         plt.show()
 
     def start_anim2(self):
-
-        # self.canvas.get_tk_widget().grid()
-
-        # toolbar = NavigationToolbar2Tk(self.canvas, self.master)
-        # toolbar.update()
-        # self.canvas._tkcanvas.pack()
-
         self.ani = animation.FuncAnimation(
-            self.fig, self.animate, frames=self.frames, interval=100, save_count=100
+            self.fig, self.draw, frames=self.frames, interval=100, save_count=100
         )
+        self.ani.pause()
 
     def frames(self):
         while True:
             yield self.controller()
 
-    xs = []
-    ys = []
-
-    def animate(self, args):
+    def draw(self, args):
         info, items, tot_reward = args
 
         self.ax.clear()
@@ -81,111 +77,7 @@ class Visualization:
         # Check if the environment is terminal
         if self.game.has_ended():
             self.draw_complete()
-
-    def draw_agent(self, info):
-        # Draw agent
-        for pos, has_item in info:
-            ax, ay = pos
-            agent_color = "blue" if not has_item else "orange"
-            agent_patch = patches.Circle((ax + 0.5, ay + 0.5), 0.3, color=agent_color)
-            self.ax.add_patch(agent_patch)
-
-    def draw_item(self, items):
-        for item in items:
-            ix, iy = item
-            item_patch = patches.Circle((ix + 0.5, iy + 0.5), 0.2, color="red")
-            self.ax.add_patch(item_patch)
-
-    def draw_complete(self):
-        self.ax.text(
-            0.5,
-            0.5,
-            "Complete",
-            horizontalalignment="center",
-            verticalalignment="center",
-            transform=self.ax.transAxes,
-            fontsize=20,
-            color="red",
-        )
-
-    # ----- ----- ----- ----- Render UI Element  ----- ----- ----- ----- #
-
-    def add_ui_elements(self):
-        self.init_buttons()
-        self.init_slider()
-        self.init_text()
-        self.update()
-
-    def init_buttons(self):
-        # Add button for next step
-        self.next_step_button = self.add_button(
-            [0.85, 0.01, 0.1, 0.075], "Next Step", self.next
-        )
-
-        # Add button for reset
-        self.reset_button = self.add_button(
-            [0.85, 0.11, 0.1, 0.075], "Reset", self.reset
-        )
-        # Add button for reset
-        self.animate_button = self.add_button(
-            [0.85, 0.21, 0.1, 0.075], "Animate", self.start_animation
-        )
-        # Add button for reset
-        self.stop_button = self.add_button([0.85, 0.31, 0.1, 0.075], "Stop", self.stop)
-
-    def init_slider(self):
-        plt.subplots_adjust(bottom=0.15)
-        axspeed = plt.axes([0.175, 0.08, 0.6, 0.03])
-        self.sspeed = Slider(axspeed, "Speed", 0.1, 10.0, valinit=1.0)
-        self.sspeed.on_changed(self.on_timeout_update)
-
-    def init_text(self):
-        # Add text box for cumulative reward
-        self.reward = self.add_text(
-            [0.01, 0.01, 0.2, 0.075], f"Reward: {self.game.total_reward}"
-        )
-
-        # Add text box for max reward
-        self.max_reward = self.add_text(
-            [0.25, 0.01, 0.2, 0.075],
-            f"Max Reward: {self.game.get_max_reward()}",
-        )
-
-    def add_button(self, coordinates: Coordinates, text, on_click):
-        axis = plt.axes(coordinates)
-        button = Button(axis, text)
-        button.on_clicked(on_click)
-
-        return button
-
-    def add_text(self, coordinates: Coordinates, text):
-        axis = plt.axes(coordinates)
-        textbox = axis.text(
-            0.5,
-            0.5,
-            text,
-            horizontalalignment="center",
-            verticalalignment="center",
-            transform=axis.transAxes,
-            fontsize=12,
-        )
-        axis.axis("off")
-        return textbox
-
-    # ----- ----- ----- ----- Render Main Board  ----- ----- ----- ----- #
-
-    def update(self):
-        self.draw_grid()
-
-        self.draw_agent(self.game.get_agent_info())
-        self.draw_item(self.game.get_untaken_items())
-        self.reward.set_text(f"Reward: {self.game.total_reward}")
-
-        # Check if the environment is terminal
-        if self.game.has_ended():
-            self.draw_complete()
-
-        self.fig.canvas.draw()
+        # self.fig.canvas.draw()
 
     def draw_grid(self):
         # self.ax.clear()
@@ -237,48 +129,128 @@ class Visualization:
             color="red",
         )
 
-    def foo(self):
-        self.one_step()
-        if not (self.is_stopping):
-            self.timer = threading.Timer(self.speed, self.foo)
-            self.timer.start()
+    # ----- ----- ----- ----- Render UI Element  ----- ----- ----- ----- #
+
+    def add_ui_elements(self):
+        self.init_buttons()
+        self.init_slider()
+        self.init_text()
+        # self.update()
+
+    def init_buttons(self):
+        # Add button for next step
+        self.next_step_button = self.add_button(
+            [0.85, 0.01, 0.1, 0.075], "Next Step", self.next
+        )
+
+        # Add button for reset
+        self.reset_button = self.add_button(
+            [0.85, 0.11, 0.1, 0.075], "Reset", self.reset
+        )
+        # Add button for reset
+        self.animate_button = self.add_button(
+            [0.85, 0.21, 0.1, 0.075], "Animate", self.start_animation
+        )
+        # Add button for reset
+        self.stop_button = self.add_button([0.85, 0.31, 0.1, 0.075], "Stop", self.stop)
+
+    def init_slider(self):
+        plt.subplots_adjust(bottom=0.15)
+        axspeed = plt.axes([0.175, 0.08, 0.6, 0.03])
+        self.sspeed = Slider(axspeed, "Speed", 0.5, 5, valinit=2.0)
+        self.sspeed.on_changed(self.on_speed_update)
+
+    def init_text(self):
+        # Add text box for cumulative reward
+        self.reward = self.add_text(
+            [0.01, 0.01, 0.2, 0.075], f"Reward: {self.game.total_reward}"
+        )
+
+        # Add text box for max reward
+        self.max_reward = self.add_text(
+            [0.25, 0.01, 0.2, 0.075],
+            f"Max Reward: {self.game.get_max_reward()}",
+        )
+
+    def add_button(self, coordinates: Coordinates, text, on_click):
+        axis = plt.axes(coordinates)
+        button = Button(axis, text)
+        button.on_clicked(on_click)
+
+        return button
+
+    def add_text(self, coordinates: Coordinates, text):
+        axis = plt.axes(coordinates)
+        textbox = axis.text(
+            0.5,
+            0.5,
+            text,
+            horizontalalignment="center",
+            verticalalignment="center",
+            transform=axis.transAxes,
+            fontsize=12,
+        )
+        axis.axis("off")
+        return textbox
+
+    # ----- ----- ----- ----- Render Main Board  ----- ----- ----- ----- #
+
+    # def update(self):
+    #     self.draw_grid()
+
+    #     self.draw_agent(self.game.get_agent_info())
+    #     self.draw_item(self.game.get_untaken_items())
+    #     self.reward.set_text(f"Reward: {self.game.total_reward}")
+
+    #     # Check if the environment is terminal
+    #     if self.game.has_ended():
+    #         self.draw_complete()
+
+    #     self.fig.canvas.draw()
+
+    # def foo(self):
+    #     self.one_step()
+    #     if not (self.is_stopping):
+    #         self.timer = threading.Timer(self.speed, self.foo)
+    #         self.timer.start()
 
     def one_step(self):
-        self.game.step(learn=False)
-        self.update()
+        self.draw(self.controller())
 
     def stop_anim(self):
-        self.is_stopping = True
-        if self.timer:
-            self.timer.cancel()
+        pass
 
     def start_anim(self):
-        self.is_stopping = False
-        self.foo()
+        pass
 
     # ----- ----- ----- ----- Event Handlers  ----- ----- ----- ----- #
     def start_animation(self, event):
-        self.start_anim()
+        self.ani.resume()
 
     def stop(self, event):
-        self.stop_anim()
+        self.ani.pause()
 
     def reset(self, event):
         # self.stop_anim()
         self.game.reset()
-        self.max_reward.set_text(f"Max Reward: {self.game.get_max_reward()}")
-        self.update()
+        self.draw(self.controller.get_info())
+        # self.max_reward.set_text(f"Max Reward: {self.game.get_max_reward()}")
+        # self.update()
 
     def next(self, e):
-        self.stop_anim()
-        self.one_step()
+        self.draw(self.controller())
+        # self.stop_anim()
+        # self.one_step()
 
     def on_close(self, e):
-        self.stop_anim()
+        pass
+        # self.stop_anim()
 
-    def on_timeout_update(self, val: float | None):
+    def on_speed_update(self, val: float | None):
         if val:
-            self.speed = 1 / val
+            self.controller.set_timeout(1 / val)
+        # if val:
+        #     self.speed = 1 / val
 
     def show(self):
         self.fig.canvas.mpl_connect("close_event", self.on_close)
