514a688da20e26cf7d4da8005566b7772e861ed8
Argens <argens1203@gmail.com>
2024-10-02 20:29:03 +1000

    style: removed commented codes

--- main.py
+++ main.py
@@ -7,23 +7,14 @@ if __name__ == "__main__":
     width, height = 4, 4
     max_itr = 1000
 
-    # controller = Controller()
-
     storage = Storage(max_itr)
-    # trainer = Trainer(max_itr)
-
     agent = Agent(
         0,
         state_size,
         [(0, -1), (0, 1), (-1, 0), (1, 0)],
     )
     grid = Grid(width, height, [agent], storage, max_itr)
-    # grid.add_agent(agent)
-
-    # trainer.bind(storage, grid, [agent])
-
     grid.reset()
-    # controller.bind(grid)
 
     grid.train(2500)
     fig2, ax2 = plt.subplots()
@@ -32,10 +23,3 @@ if __name__ == "__main__":
     fig1, ax1 = plt.subplots()
     vis = Visualization(fig1, ax1)
     vis.bind(storage, grid).show()
-
-    # game = Game()
-    # controller = Controller(game, 1000)
-    # controller.train(1000)
-    # fig1, ax1 = plt.subplots()
-    # vis = Visualization(game, controller, fig1, ax1)
-    # Visualization.plot_training(controller.get_metrics())
